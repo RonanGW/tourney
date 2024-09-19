127bb5956d05011048e38e00ae9d82bd694829cb
Ronan Wolfe <ronangwolfe@gmail.com>
2024-09-18 20:55:30 -0700

    Estabish character data structure. Improve styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { parseJsonText } from 'typescript'
 import './App.css'
 import CharCard from './CharCard'
+import tdata from './trainers.json'
 
 function App() {
+
   return (
     <div className="App">
       <div className="Content">
-        <CharCard></CharCard>
+        <CharCard trainer={tdata.red}></CharCard>
       </div>
     </div>
   );
--- src/charCard.tsx
+++ src/charCard.tsx
@@ -1,11 +1,16 @@
 import './CharCard.css'
 
-export default function CharCard() {
+interface CharCard {
+  trainer: any;
+}
+
+const CharCard = ({trainer}: CharCard) => {
   return (
     <div className='card'>
-      <img src='./chars/ppl/Red.png'></img>
+      <img src='./chars/ppl/Red.png' className='cardImg'></img>
+      {trainer.name}
     </div>
   );
 }
 
-
+export default CharCard
