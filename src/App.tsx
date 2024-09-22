e3e8f2671ac4e2b7e4e8af13b39098528fae42e2
Ronan Wolfe <ronangwolfe@gmail.com>
2024-09-22 15:58:38 -0700

    Added Main Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import Gallery from './Gallery/Gallery'
+import "./index.css"
 
 function App() {
+  const [menu, setMenu] = useState("Main-Menu")
 
   return (
-    <div>
-      <Gallery></Gallery>
+    <div className='App'>
+      {menu == "Main-Menu" ? 
+        <div>
+          <button>
+            Start Round
+          </button>
+          <button onClick={() => {setMenu("Gallery")}}>
+            Gallery
+          </button>
+          <button>
+            Settings
+          </button>
+          <button>
+            Custom Add-ons
+          </button>
+        </div>:
+      menu == "Gallery" || menu == "Gallery-Trainer" ?
+        <Gallery menu={[menu,setMenu]}></Gallery> :
+      <div>Oops. Something broke :/</div>}
     </div>
   );
 }
--- src/Gallery/Gallery.tsx
+++ src/Gallery/Gallery.tsx
@@ -4,6 +4,10 @@ import CharCard from './TrainerPage/CharCard'
 import MonCard from './MonPage/MonCard'
 import tdata from '../trainers.json'
 
+interface Gallery {
+  menu: any[]
+}
+
 interface mon {
   state: string;
   lvl: number;
@@ -23,8 +27,7 @@ interface trainer {
       mons: object
 };
 
-function Gallery() {
-    const [menu, setMenu] = useState("Gallery")
+function Gallery({menu}: Gallery) {
     const [filters, setFilters] = useState(["Unlocked","Available","Locked"]);
     const [cards, setCards] = useState(filterTrainerCards());
     const [currTrainer, setCurrTrainer] = useState(tdata["red"]);
@@ -61,19 +64,19 @@ function Gallery() {
   }
 
   function switchToMainScreen() {
-    setMenu("Gallery");
+    menu[1]("Gallery");
     setSelectedMon({state:"blank",lvl:0,xp:0,hp:0,atk:0,cost:999})
     setCards(filterTrainerCards());
   }
 
   function switchToTrainerScreen(trainer: any) {
-    setMenu("Trainer");
+    menu[1]("Gallery-Trainer");
     setCurrTrainer(trainer)
     setCards(filterMonCards(trainer));
   }
 
   function switchToMonScreen(mon: mon) {
-    setMenu("Mon");
+    menu[1]("Mon");
   }
 
   function purchaseMon(trainer: trainer, mon: mon): void {
@@ -132,15 +135,12 @@ function Gallery() {
         <div className="Gallery-header">
           <button onClick={switchToMainScreen}>Back to Gallery Main Screen</button>
         </div>
-        {menu == "Gallery" ? 
+        {menu[0] == "Gallery" ? 
             <div className="Card-block">
               {cards}
             </div> : 
-         menu == "Trainer" ? 
-            TrainerPage(currTrainer) : 
-         menu == "Mon" ? 
-            <div className="TrainerCardList"> 
-            </div> :
+         menu[0] == "Gallery-Trainer" ? 
+            TrainerPage(currTrainer) :
          <div>Oops. Something broke :/</div>
         }
     </div>
