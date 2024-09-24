2e73fb2d52e41d8c90ff7c20a1e0f67d2e7f3a21
Ronan Wolfe <ronangwolfe@gmail.com>
2024-09-23 17:08:40 -0700

    Added temporary download method for save data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Gallery from './Gallery/Gallery'
+import Settings from './Settings/Settings'
 import "./index.css"
 
 function App() {
@@ -9,21 +10,22 @@ function App() {
     <div className='App'>
       {menu == "Main-Menu" ? 
         <div>
-          <button>
-            Start Round
+          <button onClick={() => {setMenu("Fight")}}>
+            Start Tourney
           </button>
           <button onClick={() => {setMenu("Gallery")}}>
             Gallery
           </button>
-          <button>
+          <button onClick={() => {setMenu("Settings")}}>
             Settings
           </button>
-          <button>
+          <button onClick={() => {setMenu("Customize")}}>
             Custom Add-ons
           </button>
         </div>:
       menu == "Gallery" || menu == "Gallery-Trainer" ?
         <Gallery menu={[menu,setMenu]}></Gallery> :
+      menu == "Settings" ? <Settings menu={[menu,setMenu]}></Settings> :
       <div>Oops. Something broke :/</div>}
     </div>
   );
---
+++ src/Settings/Settings.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react'
+import './Settings.css'
+
+interface Settings {
+  menu: any[]
+}
+
+
+
+
+
+
+function resetSave() {
+
+  let trainersSaveData: any = {}
+
+  fetch('./trainers.json')
+     .then(res => res.json())
+     .then((trainersSaveData) => {
+        console.log(trainersSaveData)
+     });
+
+  //["Kanto","Johto","Hoenn","Sinnoh","Unova","Kalos","Alola","Galar","Paldea","Hisui","Ransei"]
+  //["Hero","Champion","Rival","Professor","Boss","Elite Four","Admin","Gym Leader"]
+  let trainers = [["Red","charmander","Kanto","Hero"],
+                  ["Blue","squirtle","Kanto","Rival"],
+                  ["Leaf","bulbasaur","Kanto","Hero"],
+                  ["Lance","dratini","Kanto","Champion"],
+                  ["Steven","beldum","Hoenn","Champion"],
+                  ["Wallace","feebas","Hoenn","Champion"],
+                  ["Cynthia","gible","Sinnoh","Champion"],
+                  ["Alder","larvesta","Unova","Champion"],
+                  ["Iris","deino","Unova","Champion"],
+                  ["Diantha","ralts","Kalos","Champion"],
+                  ["Kukui","rockruff","Alola","Professor"],
+                  ["Leon","charmander","Galar","Champion"],
+                  ["Geeta","glimmet","Paldea","Champion"]
+                 ]
+
+  let mons = [{name: "Bulbasaur", cost: 2},
+              {name: "Charmander", cost: 2}
+             ]
+
+  trainers.forEach(trainer => {
+    trainersSaveData[trainer[0].toLowerCase()] = {
+        name: trainer[0],
+        state: "Locked",
+        class: trainer[3],
+        region: trainer[2],
+        w: 0,
+        l: 0,
+        BP: 0,
+        mons: {}
+    }
+
+    mons.forEach(mon => {
+        trainersSaveData[trainer[0].toLowerCase()].mons[mon.name.toLowerCase()] = {
+            name: mon.name,
+            state: "Locked",
+            lvl: 1,
+            xp: 0,
+            hp: 1,
+            atk: 1,
+            cost: mon.cost
+        }
+    })
+  });
+  //const file = writeFileSync('./trainers.json',trainersSaveData);
+  //let data = fs.writeFileSync('./trainers.json',trainersSaveData)
+
+  return trainersSaveData
+}
+
+function Settings({menu}: Settings) {
+    
+  const [tmp,setTmp] = useState("N/A")
+
+    return (
+    <div className="Settings">
+        <button onClick={() => {
+
+
+  const blob = new Blob([JSON.stringify(resetSave())], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', 'trainers.json'); // Set the desired file name
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+
+
+
+        }}>Reset Save data</button>
+        <p>{tmp}</p>
+    </div>
+  );
+}
+
+export default Settings
