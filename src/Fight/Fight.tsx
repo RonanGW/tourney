51e8c307f9110e54f36d2687721a9578651000bb
Ronan Wolfe <ronangwolfe@gmail.com>
2024-09-30 17:36:41 -0700

    imited each round to include 8 Chars

--- src/Fight/Fight.tsx
+++ src/Fight/Fight.tsx
@@ -30,33 +30,34 @@ interface trainer {
 
 
 function Fight({menu, trainers}: Fight) {
-    const [allTrainers, setActiveTrainers] = useState<[string,trainer][]>(Object.entries(trainers[0]));
-    const [remainingTrainers, setRemainingTrainers] = useState<[string,trainer][]>(allTrainers.filter((t) => t[1].state == "Unlocked"))
+    const [allTrainers, setAllTrainers] = useState<[string,trainer][]>(Object.entries(trainers[0]));
+    const [remainingTrainers, setRemainingTrainers] = useState<[string,trainer][]>((allTrainers).filter((t) => t[1].state == "Hidden"))
+    let tmp = remainingTrainers.sort(() => 0.5 - Math.random())
+    tmp = remainingTrainers.slice(0, 8)
 
 
     const x = useMotionValue(0);
     const rotate = useTransform(x, [-150, 150], [-90, 90]);
-    const angle = 360 / 8;
+    const angle = 360 / 16;
     const rotation = React.useRef(angle);
     let divs = [];
   
-    for (let [tkey,value] of remainingTrainers) {
+    for (let [tkey,value] of tmp) {
         rotation.current = rotation.current + angle
+        let imgURL='./chars/ppl/'+ value.name.toLowerCase()+'.png'
 
         divs.push(
         <div
         style={{
-            transform: `rotate(${
-              rotation.current
-            }deg) translate(${250}px) rotate(${
-              rotation.current * -1
-            }deg)  ${
-                trainers[0]["red"].name === value.name ? 'scale(1.5)' : ''
-            }`,
+            transform: `rotate(${rotation.current}deg)
+                        translate(${250}px)
+                        rotate(${rotation.current * -1}deg)
+                        ${trainers[0]["red"].name === value.name ? 'scale(1.5)' : ''}`,
             margin: '-40px',
           }}
-        >{value.name}</div>)
+        ><img src={imgURL}></img></div>)
         }
+
     return (
     <div>
         <div className="Gallery-header">
@@ -69,11 +70,6 @@ function Fight({menu, trainers}: Fight) {
             }}
             className="relative border border-4 border-red-900 rounded-full h-[500px] w-[500px] bg-red-200"
             >
-            <figure className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-            <caption className="block text-gray-800 mt-2 text-lg">
-            {trainers[0]["red"].name} - {trainers[0]["red"].class}
-            </caption>
-            </figure>
 
             {divs}
             
