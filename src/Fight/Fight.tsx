0c3f9900dc96054b0e643783cb8f5ef3ab5c1fe9
Ronan Wolfe <ronangwolfe@gmail.com>
2024-10-16 21:50:59 -0700

    Restructured card generation to use functions & improved HP decrease function. Works correctly the first instance now.

--- src/Fight/Fight.tsx
+++ src/Fight/Fight.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './Fight.css'
+import { reverse } from 'dns';
 
 interface Fight {
   menu: any[]
@@ -36,34 +37,47 @@ function Fight({menu, trainers}: Fight) {
                                                                         .sort(() => 0.5 - Math.random()))
                                                                         .slice(0, 8)))
     const [activeTrainer, setActiveTrainer] = useState(Object.keys(currTrainers)[0])
-    let leftTrainers = [];
-    let rightTrainers = [];
-    let evenIndex = true
+    const [fullCards, setFullCards] = useState(<div
+        className='flexRow'
+        style={{justifyContent:"space-between",marginLeft:"10vw",marginRight:"10vw",height:"75vh"}}>
+           <div className='flexCol'
+           style={{justifyContent:"space-between"}}>
+               {setTrainerCards(true, true)}
+           </div>
+           <div className='flexCol'
+           style={{justifyContent:"space-between"}}>
+               {setTrainerCards(true, false)}
+           </div>
+       </div>)
+    // const [rightTrainers, setRightTrainers] = useState([])
+    // let tmpleftTrainers = [];
+    // let tmprightTrainers = [];
+    // let evenIndex = true
     
-    for (const tKey in currTrainers) {
-        currTrainers[tKey].mons[currTrainers[tKey].starter].currHP = currTrainers[tKey].mons[currTrainers[tKey].starter].hp
-        let tImgURL='./chars/ppl/'+ currTrainers[tKey].name.toLowerCase()+'.png'
-        let mImgURL='./chars/mons/'+ currTrainers[tKey].starter.toLowerCase()+'.png'
+    // for (const tKey in currTrainers) {
+    //     currTrainers[tKey].mons[currTrainers[tKey].starter].currHP = currTrainers[tKey].mons[currTrainers[tKey].starter].hp
+    //     let tImgURL='./chars/ppl/'+ currTrainers[tKey].name.toLowerCase()+'.png'
+    //     let mImgURL='./chars/mons/'+ currTrainers[tKey].starter.toLowerCase()+'.png'
 
-        evenIndex ?
-        leftTrainers.push(<div className='flexRow'>
-                            <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}><img src={tImgURL}></img>{currTrainers[tKey].name}</div>
-                            <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}>
-                                <img src={mImgURL}></img>{currTrainers[tKey].mons[currTrainers[tKey].starter].currHP}
-                                 / 
-                                {currTrainers[tKey].mons[currTrainers[tKey].starter].hp}
-                            </div>
-                          </div>) :
-        rightTrainers.push(<div className='flexRow'>
-                            <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}><img src={mImgURL}>
-                                </img>{currTrainers[tKey].mons[currTrainers[tKey].starter].currHP}
-                                 / 
-                                {currTrainers[tKey].mons[currTrainers[tKey].starter].hp}
-                            </div>
-                            <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}><img src={tImgURL}></img>{currTrainers[tKey].name}</div>
-                           </div>)
-        evenIndex ? evenIndex = false : evenIndex = true
-    }
+    //     evenIndex ?
+    //     tmpleftTrainers.push(<div className='flexRow' onClick={() => {hp(tKey,2)}}>
+    //                         <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}><img src={tImgURL}></img>{currTrainers[tKey].name}</div>
+    //                         <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}>
+    //                             <img src={mImgURL}></img>{currTrainers[tKey].mons[currTrainers[tKey].starter].currHP}
+    //                              / 
+    //                             {currTrainers[tKey].mons[currTrainers[tKey].starter].hp}
+    //                         </div>
+    //                       </div>) :
+    //     tmprightTrainers.push(<div className='flexRow'>
+    //                         <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}><img src={mImgURL}>
+    //                             </img>{currTrainers[tKey].mons[currTrainers[tKey].starter].currHP}
+    //                              / 
+    //                             {currTrainers[tKey].mons[currTrainers[tKey].starter].hp}
+    //                         </div>
+    //                         <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}><img src={tImgURL}></img>{currTrainers[tKey].name}</div>
+    //                        </div>)
+    //     evenIndex ? evenIndex = false : evenIndex = true
+    // }
 
     // for (let [tkey,value] of remainingTrainers) {
     //     value.mons[value.starter].currHP = 1
@@ -82,15 +96,58 @@ function Fight({menu, trainers}: Fight) {
     //     index++
     // }
 
+    function setTrainerCards(start: boolean, left: boolean): JSX.Element[] {
+        let trainers: JSX.Element[] = [];
+        let index = 0
+        
+        
+        for (const tKey in currTrainers) {
+            if ((left && index < Object.keys(currTrainers).length / 2 || !left && index > Object.keys(currTrainers).length / 2 - 1)) {
+                if (start) {currTrainers[tKey].mons[currTrainers[tKey].starter].currHP = currTrainers[tKey].mons[currTrainers[tKey].starter].hp}
+                let tImgURL='./chars/ppl/'+ currTrainers[tKey].name.toLowerCase()+'.png'
+                let mImgURL='./chars/mons/'+ currTrainers[tKey].starter.toLowerCase()+'.png'
+        
+                trainers.push(<div className='flexRow' style={{display: "flex", flexDirection: left ? "row" : "row-reverse"}} onClick={() => {hp(tKey,2);setFullCards(combineLists)}}>
+                                    <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}><img src={tImgURL}></img>{currTrainers[tKey].name}</div>
+                                    <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}>
+                                        <img src={mImgURL}></img>{currTrainers[tKey].mons[currTrainers[tKey].starter].currHP}
+                                        / 
+                                        {currTrainers[tKey].mons[currTrainers[tKey].starter].hp}
+                                    </div>
+                                </div>)
+            }
+            index++
+            //style={{display: "flex", flexDirection: index > Object.keys(currTrainers).length / 2 ? "row-reverse" : "row"}}
+        }
+
+        return trainers
+    }
+
+    function combineLists(): JSX.Element {
+       return (
+        <div
+         className='flexRow'
+         style={{justifyContent:"space-between",marginLeft:"10vw",marginRight:"10vw",height:"75vh"}}>
+            <div className='flexCol'
+            style={{justifyContent:"space-between"}}>
+                {setTrainerCards(false, true)}
+            </div>
+            <div className='flexCol'
+            style={{justifyContent:"space-between"}}>
+                {setTrainerCards(false, false)}
+            </div>
+        </div>)
+    }
+
     function hp(trainer: string, diff: number) {
        let result = currTrainers[trainer].mons[currTrainers[trainer].starter].currHP - diff
        result < 0 ? 
         result = 0 : 
-        result > currTrainers[trainer].mons[currTrainers[trainer].starter].hp  ? 
+        result > currTrainers[trainer].mons[currTrainers[trainer].starter].hp ? 
             result = currTrainers[trainer].mons[currTrainers[trainer].starter].hp :
             result = result
        let tmp = currTrainers
-       tmp[trainer].mons[currTrainers[trainer].starter].currHP = result
+       tmp[trainer].mons[tmp[trainer].starter].currHP = result
        setCurrTrainers(tmp)
     }
 
@@ -99,18 +156,7 @@ function Fight({menu, trainers}: Fight) {
         <div className="Fight-header">
             <button onClick={() => {menu[1]("Main-Menu")}}>Back Main Menu</button>
         </div>
-        <div
-        className='flexRow'
-        style={{justifyContent:"space-between",marginLeft:"10vw",marginRight:"10vw",height:"75vh"}}>
-            <div className='flexCol'
-            style={{justifyContent:"space-between"}}>
-                {leftTrainers}
-            </div>
-            <div className='flexCol'
-            style={{justifyContent:"space-between"}}>
-                {rightTrainers}
-            </div>
-        </div>
+        {fullCards}
     </div>
     )
 }
