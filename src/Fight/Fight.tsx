289f8c842eff11a0b0e3ff2e8af355ff9ed332a1
Ronan Wolfe <ronangwolfe@gmail.com>
2024-10-17 22:05:03 -0700

    Improved debugging and restructuring to identify bug of each fight column being called twice. Also fixed non-unique key issues

--- src/Fight/Fight.tsx
+++ src/Fight/Fight.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import './Fight.css'
-import { reverse } from 'dns';
 
 interface Fight {
   menu: any[]
@@ -37,18 +36,7 @@ function Fight({menu, trainers}: Fight) {
                                                                         .sort(() => 0.5 - Math.random()))
                                                                         .slice(0, 8)))
     const [activeTrainer, setActiveTrainer] = useState(Object.keys(currTrainers)[0])
-    const [fullCards, setFullCards] = useState(<div
-        className='flexRow'
-        style={{justifyContent:"space-between",marginLeft:"10vw",marginRight:"10vw",height:"75vh"}}>
-           <div className='flexCol'
-           style={{justifyContent:"space-between"}}>
-               {setTrainerCards(true, true)}
-           </div>
-           <div className='flexCol'
-           style={{justifyContent:"space-between"}}>
-               {setTrainerCards(true, false)}
-           </div>
-       </div>)
+    const [fullCards, setFullCards] = useState(combineLists(true))
     // const [rightTrainers, setRightTrainers] = useState([])
     // let tmpleftTrainers = [];
     // let tmprightTrainers = [];
@@ -96,18 +84,40 @@ function Fight({menu, trainers}: Fight) {
     //     index++
     // }
 
+    function combineLists(start: boolean): JSX.Element {
+        return (
+         <div
+          className='flexRow'
+          style={{justifyContent:"space-between",marginLeft:"10vw",marginRight:"10vw",height:"75vh"}}>
+             <div className='flexCol'
+             style={{justifyContent:"space-between"}}>
+                 {setTrainerCards(start, true)}
+             </div>
+             <div className='flexCol'
+             style={{justifyContent:"space-between"}}>
+                 {setTrainerCards(start, false)}
+             </div>
+         </div>)
+     }
+
     function setTrainerCards(start: boolean, left: boolean): JSX.Element[] {
         let trainers: JSX.Element[] = [];
         let index = 0
+        console.log(Object.keys(currTrainers))
         
         
         for (const tKey in currTrainers) {
-            if ((left && index < Object.keys(currTrainers).length / 2 || !left && index > Object.keys(currTrainers).length / 2 - 1)) {
+            console.log(tKey)
+            if ((left && index < Object.keys(currTrainers).length / 2) || (!left && index > Object.keys(currTrainers).length / 2 - 1)) {
+                //console.log(tKey)
+                //console.log(currTrainers[tKey].mons[currTrainers[tKey].starter].currHP)
                 if (start) {currTrainers[tKey].mons[currTrainers[tKey].starter].currHP = currTrainers[tKey].mons[currTrainers[tKey].starter].hp}
+                //console.log(currTrainers[tKey].mons[currTrainers[tKey].starter].currHP)
                 let tImgURL='./chars/ppl/'+ currTrainers[tKey].name.toLowerCase()+'.png'
                 let mImgURL='./chars/mons/'+ currTrainers[tKey].starter.toLowerCase()+'.png'
+                console.log(tImgURL)
         
-                trainers.push(<div className='flexRow' style={{display: "flex", flexDirection: left ? "row" : "row-reverse"}} onClick={() => {hp(tKey,2);setFullCards(combineLists)}}>
+                trainers.push(<div key={Math.random()} className='flexRow' style={{display: "flex", flexDirection: left ? "row" : "row-reverse"}} onClick={() => {hp(tKey,2);setFullCards(combineLists(false))}}>
                                     <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}><img src={tImgURL}></img>{currTrainers[tKey].name}</div>
                                     <div className='flexCol' style={{width:"128px",backgroundColor:tKey == activeTrainer ? "green":"blue"}}>
                                         <img src={mImgURL}></img>{currTrainers[tKey].mons[currTrainers[tKey].starter].currHP}
@@ -115,28 +125,17 @@ function Fight({menu, trainers}: Fight) {
                                         {currTrainers[tKey].mons[currTrainers[tKey].starter].hp}
                                     </div>
                                 </div>)
+            console.log("-----------------------------")
+            }
+            else {
+                console.log("Not Pushed")
+                console.log("-----------------------------")
             }
             index++
             //style={{display: "flex", flexDirection: index > Object.keys(currTrainers).length / 2 ? "row-reverse" : "row"}}
         }
 
         return trainers
-    }
-
-    function combineLists(): JSX.Element {
-       return (
-        <div
-         className='flexRow'
-         style={{justifyContent:"space-between",marginLeft:"10vw",marginRight:"10vw",height:"75vh"}}>
-            <div className='flexCol'
-            style={{justifyContent:"space-between"}}>
-                {setTrainerCards(false, true)}
-            </div>
-            <div className='flexCol'
-            style={{justifyContent:"space-between"}}>
-                {setTrainerCards(false, false)}
-            </div>
-        </div>)
     }
 
     function hp(trainer: string, diff: number) {
@@ -147,7 +146,12 @@ function Fight({menu, trainers}: Fight) {
             result = currTrainers[trainer].mons[currTrainers[trainer].starter].hp :
             result = result
        let tmp = currTrainers
-       tmp[trainer].mons[tmp[trainer].starter].currHP = result
+       //console.log("tmp["+trainer+"].mons["+tmp[trainer].starter+"].currHP")
+       //console.log(tmp[trainer].mons[tmp[trainer].starter].currHP)
+       //tmp[trainer].mons[tmp[trainer].starter].currHP = result
+       tmp[trainer].mons[tmp[trainer].starter].currHP = 5;
+      // console.log(tmp[trainer].mons[tmp[trainer].starter].currHP)
+       //console.log(tmp)
        setCurrTrainers(tmp)
     }
 
